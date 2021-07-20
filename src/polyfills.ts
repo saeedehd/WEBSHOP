e5ef5dcbcdc545dd12de6b6d66aa13672df6f2ee
SaeedehDeldar <saeedeh.deldar@gmail.com>
2021-07-20 23:48:46 +0430

    config project

---
+++ src/polyfills.ts
@@ -0,0 +1,6 @@
+/**
+* Required to support Web Animations `@angular/platform-browser/animations`.
+* Needed for: All but Chrome, Firefox and Opera. https://caniuse.com/web-animation
+**/
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'zone.js/dist/zone';
